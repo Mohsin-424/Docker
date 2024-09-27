c8f3209b97852896374247c521e931e5dae34e99
Mohsin-424 <mohsinpu24@gmail.com>
2024-09-27 10:36:40 +0500

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,6 @@ RUN pip install -r requirements.txt
 # Copy the rest of the application
 COPY . .
 
-# Expose the port that the Flask app runs on
 EXPOSE 5000
 
 # Run the Flask app
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# pip install flask
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Flask Docker App!</h1>"
+    return "<h1>Hello from Flask Docker App Mohsin!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
